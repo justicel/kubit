40c6461ce995ff13455620d2a249bbcdd65a06b2
Michael Goodness <mgoodness@gmail.com>
2017-03-30 14:03:34 -0500

    v1.5.6

--- ingress-dns.tf
+++ ingress-dns.tf
@@ -4,7 +4,7 @@ resource "aws_route53_record" "external_wildcard" {
   zone_id = "${module.common_dns.domain_zone_ids["external"]}"
   alias {
     evaluate_target_health = true
-    name = "a61c63009feb111e699d9127022d2e27-1267465812.us-east-1.elb.amazonaws.com"
+    name = "a2ee530e2157311e7becf12e68b3debd-860814901.us-east-1.elb.amazonaws.com"
     zone_id = "Z35SXDOTRQ7X7K"
   }
 }
@@ -15,7 +15,7 @@ resource "aws_route53_record" "internal_wildcard" {
   zone_id = "${module.common_dns.domain_zone_ids["internal"]}"
   alias {
     evaluate_target_health = true
-    name = "a61c63009feb111e699d9127022d2e27-1267465812.us-east-1.elb.amazonaws.com"
+    name = "a2ee530e2157311e7becf12e68b3debd-860814901.us-east-1.elb.amazonaws.com"
     zone_id = "Z35SXDOTRQ7X7K"
   }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -2,5 +2,5 @@ assets_bucket_name = "kubit-assets"
 aws_region = "us-east-1"
 cluster = { name = "kubit" }
 domain_names = { external = "aws.opsgoodness.net", internal = "kubit.local" }
-hyperkube = { version = "v1.5.3_coreos.0" }
+hyperkube = { version = "v1.5.6_coreos.0" }
 kms_key_id = ""
